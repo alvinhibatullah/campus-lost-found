f3dae8b9de74166cc4cbccc287a591a32c7b5bbd
Alvin <web.alvinsite@gmail.com>
2026-01-04 21:35:20 +0700

    Alvin migrate punya arenko

--- database/migrations/2025_12_28_144651_create_found_items_table.php
+++ database/migrations/2025_12_28_144651_create_found_items_table.php
@@ -11,14 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-      Schema::create('found_items', function (Blueprint $table) {
-    $table->id();
-    $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-    $table->string('item_name');
-    $table->text('description')->nullable();
-    $table->date('found_date');
-    $table->string('location');
-    $table->timestamps();
+     Schema::create('found_items', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+        $table->string('nama_barang'); 
+        $table->string('lokasi_ditemukan');
+        $table->date('tanggal_ditemukan');
+        $table->text('deskripsi');
+        $table->string('foto_barang')->nullable();
+        $table->string('koordinat_lokasi');
+        $table->string('status')->default('Unclaimed');
+        $table->timestamps();
 });
 
 
